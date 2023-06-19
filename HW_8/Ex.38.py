879f9519a5e54447d2182ea1ed72a0ff1bd39c1c
Vladimir.Kaydashov <kajdashov@gmail.com>
2023-06-19 16:05:48 +0300

    in the process

---
+++ HW_8/Ex.38.py
@@ -0,0 +1,21 @@
+# Задача 38: 
+# Дополнить телефонный справочник возможностью изменения и удаления данных. 
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
+
+# Показывает информацию в файле
+def show_data(filename):
+    print("\nПП | ФИО | Телефон")
+    with open(filename, "r", encoding="utf-8") as data:
+        print(data.read())
+    print("")
+
+# Записывает информацию в файл
+def export_data(filename):
+    with open(filename, "r", encoding="utf-8") as data:
+        tel_file = data.read()
+    num = len(tel_file.split("\n"))
+    with open(filename, "a", encoding="utf-8") as data: 
+        fio = input("Введите ФИО: ")
+        phone_number = input("Введите номер телефона: ")
+        data.write(f"{num} | {fio} | {phone_number}\n")
+        print(f"Добавлена запись : {num} | {fio} | {phone_number}\n")
